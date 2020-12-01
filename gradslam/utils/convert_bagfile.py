4f744c54605980aa6e5a5ef3e3b6a04db0fe6597
chuong98vt <chuong98vt@gmail.com>
2020-11-30 19:42:19 -0700

    ok

--- gradslam/utils/convert_bagfile.py
+++ gradslam/utils/convert_bagfile.py
@@ -8,86 +8,154 @@ import numpy as np
 import cv2
 import argparse
 import os
-# from mmcv import ProgressBar
+import shutil
+from mmcv import ProgressBar
 
 def parse_args():
     # Create object for parsing command-line options
     parser = argparse.ArgumentParser(description="Read recorded bag file and display depth stream in jet colormap.\
                                     Remember to change the stream resolution, fps and format to match the recorded.")
     # Add argument which takes path to a bag file as an input
-    parser.add_argument("input", type=str, help="Path to the bag file")
-    parser.add_argument("--outdir", type=str, default=None, help="Output directory")
+    parser.add_argument("input", type=str, help="Path to the bag file. Set to None to get from Camera")
+    parser.add_argument("--depth-resolution", choices=['low','vga', 'xga'], default='xga', 
+                        help="Depth resolution: low(320x240), vga(640x480), xga (1024x768).\
+                            If data is loaded from a bag-file, it must be consistent with recording condition.")
+    parser.add_argument("--rgb-resolution", choices=['vga','hd', 'fhd'], default='hd', 
+                        help="RGB resolution: vga(960x540), hd(1280x720), fhd(1920x1080).\
+                            If data is loaded from a bag-file, it must be consistent with recording condition.")
+    parser.add_argument("--color-mode", choices=['rgb8','bgr8'], default='rgb8', 
+                        help="The color mode when collecting data.\
+                            If data is loaded from a bag-file, it must be consistent with recording condition.")                        
+    parser.add_argument("--align", choices=['to_depth','to_color'], default='to_color', 
+                        help="Align Depth and RGB images together: to_depth(RGB is align to Depth image), and vice versa.\
+                            If data is loaded from a bag-file, it must be consistent with recording condition.")
+    parser.add_argument("--threshold-distance", type=float, default=None, 
+                        help="clipping distance (meter) for visualization. Objects farther than this distance is ignored.")
     parser.add_argument("--num_frames", type=int, default=None, 
                         help="Number of frames to stream. Will stream all data if None.")
+    parser.add_argument("--outdir", type=str, default=None, help="Output directory")
     parser.add_argument("--show", action='store_true', help='show the video')
 
     # Parse the command line arguments to an object
     args = parser.parse_args()
     return args
 
-if __name__ == "__main__":
-    args = parse_args()
-
+def setup_pipeline(args):
     # Check if the given file have bag extension
     assert os.path.isfile(args.input), f'file {args.input} does not exit' 
     assert os.path.splitext(args.input)[1] == ".bag", "Only .bag files are accepted"
 
-    # Create pipeline
+     # Create pipeline and config
     pipeline = rs.pipeline()
-
-    # Create a config object
     config = rs.config()
+
     # Tell config that we will use a recorded device from filem to be used by the pipeline through playback.
     rs.config.enable_device_from_file(config, args.input)
-    # Configure the pipeline to stream the depth stream
-    config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30)
-    # config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
+    # Configure the pipeline to stream the depth & color 
+    if args.depth_resolution=='vga':
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    elif args.depth_resolution=='xga':
+        config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30)
+    else:
+        config.enable_stream(rs.stream.depth, 320, 240, rs.format.z16, 30)
+    
+    color_mode= rs.format.rgb8 if args.color_mode=='rgb8' else rs.format.bgr8
+    if args.rgb_resolution=='hd':
+        config.enable_stream(rs.stream.color, 1280, 720, color_mode, 30)
+    elif args.rgb_resolution=='fhd':
+        config.enable_stream(rs.stream.color, 1920, 1080, color_mode, 30)
+    else:
+        config.enable_stream(rs.stream.color, 960, 540, color_mode, 30)
 
     # Start streaming from file
-    pipeline.start(config)
+    try:
+        profile = pipeline.start(config)
+    except ValueError:
+        print("The config does not match with bag-file recorded conditiion.\
+             Please check your depth-resolution,rgb-resolution or color mode,etc")
 
-    # Create colorizer object
-    colorizer = rs.colorizer()
+    clipping_distance=None
+    if args.threshold_distance:
+        # Getting the depth sensor's depth scale (see rs-align example for explanation)
+        depth_sensor = profile.get_device().first_depth_sensor()
+        depth_scale = depth_sensor.get_depth_scale()
+        print("Depth Scale is: " , depth_scale)
+        clipping_distance = args.clipping_distance_in_meters / depth_scale
+    
+    # Create an align object
+    align_to = rs.stream.depth if args.align=='to_depth' else rs.stream.color
+    align = rs.align(align_to)
+
+    return pipeline, align, clipping_distance
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Setup camera
+    pipeline, align, clipping_distance = setup_pipeline(args)
+
+    if args.outdir:
+        if os.path.exists(args.outdir):
+            shutil.rmtree(args.outdir)
+        os.makedirs(args.outdir,exist_ok=True)
+        os.makedirs(os.path.join(args.outdir,'Depth'),exist_ok=True)
+        os.makedirs(os.path.join(args.outdir,'RGB'),exist_ok=True)
 
     # Streaming loop
     frame_i=0
-    if args.outdir:
-        os.makedirs(args.outdir,exist_ok=True)
-    
-    # Create opencv window to render image in
-    if args.show:
-        cv2.namedWindow("Depth Stream", cv2.WINDOW_AUTOSIZE)
-
-    
-    # progress = ProgressBar(task_num=args.num_frames if args.num_frames else 1e5)
+    # progress = ProgressBar(task_num=args.num_frames if args.num_frames else 1e3)
+    progress = ProgressBar(task_num=1e1)
     while True:
-        # Get frameset of depth
         frames = pipeline.wait_for_frames()
 
-        # Get depth frame
-        depth_frame = frames.get_depth_frame()
+        # Align the depth frame and color frame
+        aligned_frames = align.process(frames)
+        depth_frame = aligned_frames.get_depth_frame() 
+        color_frame = aligned_frames.get_color_frame()
 
-        
-        # Colorize depth frame to jet colormap
-        depth_color_frame = colorizer.colorize(depth_frame)
+        if not depth_frame or not color_frame:
+            continue
 
-        # Convert depth_frame to numpy array to render image in opencv
-        depth_color_image = np.asanyarray(depth_color_frame.get_data())
-        print(frame_i, depth_color_image.max())
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        if args.color_mode=='rgb8':
+            color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
         if args.outdir:
-            filename=os.path.join(args.outdir,f'{frame_i}.png')
-            cv2.imwrite(filename,depth_color_image)
+            filename_depth=os.path.join(args.outdir,f'Depth/{frame_i}.png')
+            filename_rgb=os.path.join(args.outdir,f'RGB/{frame_i}.png')
+            cv2.imwrite(filename_depth,depth_image)
+            cv2.imwrite(filename_rgb,color_image)
+            # import pdb; pdb.set_trace()
+            # check_depth=cv2.imread(filename_depth,cv2.IMREAD_UNCHANGED)
+            # print((depth_image-check_depth).abs().sum())
+
 
         if args.show:
-            # Render image in opencv window
-            cv2.imshow("Depth Stream", depth_color_image)
-            key = cv2.waitKey(1)
-            # if pressed escape exit program
-            if key == 27:
+            # Remove background - Set pixels further than clipping_distance to grey
+            grey_color = 153
+            depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+            mask = (depth_image_3d <0) 
+            if clipping_distance:
+                mask = mask | (depth_image_3d > clipping_distance)
+            bg_removed = np.where(mask, grey_color, color_image)
+
+            # Render images
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            images = np.hstack((bg_removed, depth_colormap))
+            cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('Align Example', images)
+            key = cv2.waitKey(1000)
+            # Press esc or 'q' to close the image window
+            if key & 0xFF == ord('q') or key == 27:
                 cv2.destroyAllWindows()
                 break
 
         frame_i+=1
         if args.num_frames and frame_i > args.num_frames:
             break
-        # progress.update()
+        if frame_i % 1e1 == 0:
+            # Reset progress bar
+            progress.completed=0
+            progress.file.flush()
+        progress.update()
---
+++ gradslam/utils/inspect_bagfile.py
@@ -0,0 +1,94 @@
+#####################################################
+##               Read bag from file                ##
+#####################################################
+
+
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import argparse
+import os
+from mmcv import ProgressBar
+
+def parse_args():
+    # Create object for parsing command-line options
+    parser = argparse.ArgumentParser(description="Read recorded bag file and display depth stream in jet colormap.\
+                                    Remember to change the stream resolution, fps and format to match the recorded.")
+    # Add argument which takes path to a bag file as an input
+    parser.add_argument("input", type=str, help="Path to the bag file")
+    parser.add_argument("--outdir", type=str, default=None, help="Output directory")
+    parser.add_argument("--num_frames", type=int, default=None, 
+                        help="Number of frames to stream. Will stream all data if None.")
+    parser.add_argument("--show", action='store_true', help='show the video')
+
+    # Parse the command line arguments to an object
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Check if the given file have bag extension
+    assert os.path.isfile(args.input), f'file {args.input} does not exit' 
+    assert os.path.splitext(args.input)[1] == ".bag", "Only .bag files are accepted"
+
+    # Create pipeline
+    pipeline = rs.pipeline()
+
+    # Create a config object
+    config = rs.config()
+    # Tell config that we will use a recorded device from filem to be used by the pipeline through playback.
+    rs.config.enable_device_from_file(config, args.input)
+    # Configure the pipeline to stream the depth stream
+    config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30)
+    # config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
+
+    # Start streaming from file
+    pipeline.start(config)
+
+    # Create colorizer object
+    colorizer = rs.colorizer()
+
+    # Streaming loop
+    frame_i=0
+    if args.outdir:
+        os.makedirs(args.outdir,exist_ok=True)
+    
+    # Create opencv window to render image in
+    if args.show:
+        cv2.namedWindow("Depth Stream", cv2.WINDOW_AUTOSIZE)
+
+    
+    # progress = ProgressBar(task_num=args.num_frames if args.num_frames else 1e5)
+    while True:
+        # Get frameset of depth
+        frames = pipeline.wait_for_frames()
+
+        # Get depth frame
+        depth_frame = frames.get_depth_frame()
+
+        
+        # Colorize depth frame to jet colormap
+        depth_color_frame = colorizer.colorize(depth_frame)
+
+        # Convert depth_frame to numpy array to render image in opencv
+        depth_color_image = np.asanyarray(depth_color_frame.get_data())
+        # print(frame_i, depth_color_image.max())
+        
+        if args.outdir:
+            filename=os.path.join(args.outdir,f'{frame_i}.png')
+            cv2.imwrite(filename,depth_color_image)
+
+        if args.show:
+            # Render image in opencv window
+            cv2.imshow("Depth Stream", depth_color_image)
+            key = cv2.waitKey(1)
+            # if pressed escape exit program
+            if key == 27:
+                cv2.destroyAllWindows()
+                break
+
+        frame_i+=1
+        if args.num_frames and frame_i > args.num_frames:
+            break
+        progress.update()
